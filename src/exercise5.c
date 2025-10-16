6466f3ed107c7bb5ba3ac6003f94532b0ab5834b
Кирилл Андреев <ponchickpetrovich@gmail.com>
2025-10-16 23:10:12 +0700

    я сделал 5 номер 2

--- src/exercise5.c
+++ src/exercise5.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    double A[10][3], B[3][10], C[10][10] = {0};
+    double a[10][3], b[3][10], c[10][10] = {0};
     int i, j, k;
     
-    for(i = 0; i < 10; i++) for(j = 0; j < 3; j++) scanf("%lf", &A[i][j]);
-    for(i = 0; i < 3; i++) for(j = 0; j < 10; j++) scanf("%lf", &B[i][j]);
+    for(i = 0; i < 10; i++)
+    for(j = 0; j < 3; j++)
+        scanf("%lf", &a[i][j]);
     
-    for(i = 0; i < 10; i++) 
-        for(j = 0; j < 10; j++) 
-            for(k = 0; k < 3; k++) 
-                C[i][j] += A[i][k] * B[k][j];
+    for(i = 0; i < 3; i++)
+    for(j = 0; j < 10; j++)
+        scanf("%lf", &b[i][j]);
     
-    for(i = 0; i < 10; i++) for(j = 0; j < 10; j++) printf("%.2f ", C[i][j]);
+    for(i = 0; i < 10; i++)
+    for(j = 0; j < 10; j++)
+    for(k = 0; k < 3; k++)
+        c[i][j] += a[i][k] * b[k][j];
+    
+    for(i = 0; i < 10; i++)
+    for(j = 0; j < 10; j++)
+        printf("%.2f ", c[i][j]);
+    
     printf("\n");
-    
     return 0;
 }
