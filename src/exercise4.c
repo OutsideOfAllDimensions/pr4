2676c660aa32e4435887f3a79df7445ac9bb3876
Кирилл Андреев <ponchickpetrovich@gmail.com>
2025-10-16 22:45:06 +0700

    я сделал 4 задание наверное номер 7

--- src/exercise4.c
+++ src/exercise4.c
@@ -5,19 +5,24 @@ int main(int argc, char** argv) {
     double x = atof(argv[1]);
     int k = atoi(argv[2]);
     double arr[10];
+    double res[10];
     
     for(int i = 0; i < 10; i++) {
         scanf("%lf", &arr[i]);
     }
     
-    for(int i = 8; i >= k; i--) {
-        arr[i+1] = arr[i];
+    for(int i = 0; i < k; i++) {
+        res[i] = arr[i];
     }
     
-    arr[k] = x;
+    res[k] = x;
+    
+    for(int i = k + 1; i < 10; i++) {
+        res[i] = arr[i - 1];
+    }
     
     for(int i = 0; i < 10; i++) {
-        printf("%.2f ", arr[i]);
+        printf("%.2f ", res[i]);
     }
     printf("\n");
     
