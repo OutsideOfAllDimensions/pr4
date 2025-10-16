a8ab076190c07776abb1abf5cc915d7ec57620e9
Кирилл Андреев <ponchickpetrovich@gmail.com>
2025-10-16 22:31:32 +0700

    я сделал 4 задание наверное

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    double x = atof(argv[1]);
+    int k = atoi(argv[2]);
+    double arr[10];
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &arr[i]);
+    }
+    for(int i = 9; i > k; i--) {
+        arr[i] = arr[i-1];
+    }
+    arr[k] = x;
+    for(int i = 0; i < 10; i++) {
+        printf("%.2f ", arr[i]);
+    }
+    printf("\n");
     return 0;
 }
