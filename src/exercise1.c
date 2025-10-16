061e623510f64d88550ab2be257abca6ac0def1a
Кирилл Андреев <ponchickpetrovich@gmail.com>
2025-10-16 21:35:33 +0700

    я сделал первое задание наверное

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    double arr[10];
+    double sum = 0;
+
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &arr[i]);
+        sum += arr[i];
+    }
+    double sr = sum / 10;
+
+    printf("%.2f", sr);
     return 0;
 }
