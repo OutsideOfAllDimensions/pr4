d8fb1e31f21418892920e3d58707362135dca4bb
Кирилл Андреев <ponchickpetrovich@gmail.com>
2025-10-16 23:07:02 +0700

    я сделал 5

--- src/exercise5.c
+++ src/exercise5.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    double A[10][3], B[3][10], C[10][10] = {0};
+    int i, j, k;
+    
+    for(i = 0; i < 10; i++) for(j = 0; j < 3; j++) scanf("%lf", &A[i][j]);
+    for(i = 0; i < 3; i++) for(j = 0; j < 10; j++) scanf("%lf", &B[i][j]);
+    
+    for(i = 0; i < 10; i++) 
+        for(j = 0; j < 10; j++) 
+            for(k = 0; k < 3; k++) 
+                C[i][j] += A[i][k] * B[k][j];
+    
+    for(i = 0; i < 10; i++) for(j = 0; j < 10; j++) printf("%.2f ", C[i][j]);
+    printf("\n");
+    
     return 0;
 }
