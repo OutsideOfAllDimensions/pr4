d610ea3ab7bdd4921cf510998f66c82b311c6644
Кирилл Андреев <ponchickpetrovich@gmail.com>
2025-10-16 22:42:26 +0700

    я сделал 4 задание наверное номер 6

--- src/exercise4.c
+++ src/exercise4.c
@@ -10,9 +10,10 @@ int main(int argc, char** argv) {
         scanf("%lf", &arr[i]);
     }
     
-    for(int i = 9; i > k; i--) {
-        arr[i] = arr[i-1];
+    for(int i = 8; i >= k; i--) {
+        arr[i+1] = arr[i];
     }
+    
     arr[k] = x;
     
     for(int i = 0; i < 10; i++) {
