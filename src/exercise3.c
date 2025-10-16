3bfd62afbe2afd4bd79847406e0c82bcb6c157b9
Кирилл Андреев <ponchickpetrovich@gmail.com>
2025-10-16 22:23:57 +0700

    я сделал третье задание наверное

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    int k = atoi(argv[1]);
+    double arr[10];
+    for(int i = 0; i < 10; i++) {
+        scanf("%lf", &arr[i]);
+    }
+    for(int i = 0; i < 10; i++) {
+        int chislo = (i - k + 100) % 10;
+        printf("%.2f ", arr[chislo]);    
+    
+    }
     return 0;
 }
