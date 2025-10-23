b8004a291704c55b18c7b5b0942dca9ea6060b52
Кирилл Андреев <ponchickpetrovich@gmail.com>
2025-10-24 01:00:42 +0700

    Ъ

--- src/array_function.c
+++ src/array_function.c
@@ -10,15 +10,15 @@ void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, i
 }
 
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
-    razmer = size_src1 + size_src1
+    join_int_arrays(src1, size_src1, src2, size_src2, dest);
+    size_t razmer = size_src1 + size_src2;
+    size_t i = 0;
     while (i < razmer) {
         if (i == 0) {
-            i++
+            i++;
         }
-        else {
-            if (dest[i] >= dest[i-1]) {
-                i++;
-            }
+        else if (dest[i] >= dest[i-1]) {
+            i++;
         }
         else {
             int tmp = dest[i];
@@ -31,7 +31,7 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
     *min = src[0];
     *max = src[0];
-    for (size_t i = 0; i < size; i++) {
+    for (size_t i = 1; i < size; i++) {
         if (src[i] < *min) {
             *min = src[i];
         }
